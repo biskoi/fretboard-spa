6563169c5b7b664baadd51a4043e9b74c9c3923e
biskoi <biskoi@daydream.cafe>
2020-12-06 09:06:51 -0700

    add todo

--- fretboard/src/App.js
+++ fretboard/src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import {useState} from 'React';
 import './App.css';
 
-function App() {
+// TODO:
+// 1. Be able to record, and store, audio
+// 2. Research audio analysis options
+// 3. Implement spaced repetition system - needs persistence, so I will definitely need to write up a server...
+
+
+function App(props) {
 
   const [audioData, setAudioData] = useState([]);
+  const [currentTask, setCurrentTask] = useState(['nothing'])
 
   return (
     <div className="App">
       <div className = 'prompt'>
         <p>note prompts go here</p>
+        <p>play a {currentTask}</p>
       </div>
       <div className = 'ui'>
         <button onClick = {() => setAudioData(1)}>start record</button>
